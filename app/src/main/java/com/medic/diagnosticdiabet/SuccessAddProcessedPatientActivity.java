e9c02f1ce04bd88132b467282e6b8c164fdb6fe2
lomatik <ralenko2012@gmail.com>
2020-02-25 16:30:40 +0200

    It`s 7th commit, where i add new activities

---
+++ app/src/main/java/com/medic/diagnosticdiabet/SuccessAddProcessedPatientActivity.java
@@ -0,0 +1,14 @@
+package com.medic.diagnosticdiabet;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SuccessAddProcessedPatientActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_success_add_processed_patient);
+    }
+}
