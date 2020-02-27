46d1c4c19b6ff23f42d2946ef3bbfa09914ec3dc
lomatik <ralenko2012@gmail.com>
2020-02-27 17:21:49 +0200

    It`s 10th commit, where i wrote code to send data from getactivity to calculate activity

--- app/src/main/java/com/medic/diagnosticdiabet/GetForCAlculateActivity.java
+++ app/src/main/java/com/medic/diagnosticdiabet/GetForCAlculateActivity.java
@@ -2,9 +2,12 @@ package com.medic.diagnosticdiabet;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class GetForCAlculateActivity extends AppCompatActivity {
     Button calc_button;
@@ -30,5 +33,54 @@ public class GetForCAlculateActivity extends AppCompatActivity {
         editText_kapi_plaz_second = findViewById(R.id.editTextkapiplazsecond);
         editText_ven_plaz_first = findViewById(R.id.editTextvenplazfirst);
         editText_ven_plaz_second = findViewById(R.id.editTextvenplazsecond);
+        back_button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent back_to_main_from_show = new Intent(GetForCAlculateActivity.this, MainActivity.class);
+                startActivity(back_to_main_from_show);
+            }
+        });
+        calc_button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                if ("".equals(editText_surname.getText().toString().trim())){
+                    Toast errorsurname = Toast.makeText(getApplicationContext(),"Заповніть поле Прізвище", Toast.LENGTH_SHORT);
+                    errorsurname.show();
+                }
+                else if ("".equals(editText_ven_krov_first.getText().toString().trim())) {
+                    Toast errorven_krov_first = Toast.makeText(getApplicationContext(),"Заповніть поле Венозна кров", Toast.LENGTH_SHORT);
+                    errorven_krov_first.show();
+                }
+                else if ("".equals(editText_ven_krov_second.getText().toString().trim())) {
+                    Toast errorven_krov_second = Toast.makeText(getApplicationContext(),"Заповніть поле Венозна кров після 2 годин", Toast.LENGTH_SHORT);
+                    errorven_krov_second.show();
+                }
+                else if ("".equals(editText_kapi_plaz_first.getText().toString().trim())) {
+                    Toast errorkapi_plaz_first = Toast.makeText(getApplicationContext(),"Заповніть поле Капілярна плазма", Toast.LENGTH_SHORT);
+                    errorkapi_plaz_first.show();
+                }
+                else if ("".equals(editText_kapi_plaz_second.getText().toString().trim())) {
+                    Toast errorkapi_plaz_second = Toast.makeText(getApplicationContext(),"Заповніть поле Капілярна плазма після 2 годин", Toast.LENGTH_SHORT);
+                    errorkapi_plaz_second.show();
+                }
+                else if ("".equals(editText_ven_plaz_first.getText().toString().trim())) {
+                    Toast errorven_plaz_first = Toast.makeText(getApplicationContext(),"Заповніть поле Венозна плазма", Toast.LENGTH_SHORT);
+                    errorven_plaz_first.show();
+                }
+                else if ("".equals(editText_ven_plaz_second.getText().toString().trim())) {
+                    Toast errorven_plaz_second = Toast.makeText(getApplicationContext(),"Заповніть поле Венозна плазма після 2 годин", Toast.LENGTH_SHORT);
+                    errorven_plaz_second.show();
+                }
+                else {
+                    Intent sendtocalc = new Intent(GetForCAlculateActivity.this,ShowResultActivity.class);
+                    sendtocalc.putExtra("send_surname",editText_surname.toString());
+                    sendtocalc.putExtra("send_ven_krov_first",editText_ven_krov_first.toString());
+                    sendtocalc.putExtra("send_ven_krov_second",editText_ven_krov_second.toString());
+                    sendtocalc.putExtra("send_kapi_plaz_first",editText_kapi_plaz_first.toString());
+                    sendtocalc.putExtra("send_kapi_plaz_second",editText_kapi_plaz_second.toString());
+                    sendtocalc.putExtra("send_ven_plaz_first",editText_ven_plaz_first.toString());
+                    sendtocalc.putExtra("send_ven_plaz_second",editText_ven_plaz_second.toString());
+                    startActivity(sendtocalc);
+                }
+            }
+        });
     }
 }
--- app/src/main/java/com/medic/diagnosticdiabet/ShowResultActivity.java
+++ app/src/main/java/com/medic/diagnosticdiabet/ShowResultActivity.java
@@ -2,7 +2,9 @@ package com.medic.diagnosticdiabet;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -25,15 +27,35 @@ public class ShowResultActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_show_result);
+        String surname = getIntent().getStringExtra("send_surname");
+        double ven_krov_first = Double.parseDouble(getIntent().getStringExtra("send_ven_krov_first"));
+        double ven_krov_second = Double.parseDouble(getIntent().getStringExtra("send_ven_krov_second"));
+        double kapi_plaz_first = Double.parseDouble(getIntent().getStringExtra("send_kapi_plaz_first"));
+        double kapi_plaz_second = Double.parseDouble(getIntent().getStringExtra("send_kapi_plaz_second"));
+        double ven_plaz_first = Double.parseDouble(getIntent().getStringExtra("send_ven_plaz_first"));
+        double ven_plaz_second = Double.parseDouble(getIntent().getStringExtra("send_ven_plaz_second"));
         add_full_button = findViewById(R.id.addfullbutton);
         back_button = findViewById(R.id.back_button);
         textView_surname = findViewById(R.id.textViewsurname);
+        textView_surname.setText(surname);
         textView_ven_krov_first = findViewById(R.id.textViewvenkrovfirst);
+        textView_ven_krov_first.setText(String.valueOf(ven_krov_first));
         textView_ven_krov_second = findViewById(R.id.textViewvenkrovsecond);
+        textView_ven_krov_second.setText(String.valueOf(ven_krov_second));
         textView_kapi_plaz_first = findViewById(R.id.textViewkapiplazfirst);
+        textView_kapi_plaz_first.setText(String.valueOf(kapi_plaz_first));
         textView_kapi_plaz_second = findViewById(R.id.textViewkapiplazsecond);
+        textView_kapi_plaz_second.setText(String.valueOf(kapi_plaz_second));
         textView_ven_plaz_first = findViewById(R.id.textViewvenplazfirst);
+        textView_ven_plaz_first.setText(String.valueOf(ven_plaz_first));
         textView_ven_plaz_second = findViewById(R.id.textViewvenplazsecond);
+        textView_ven_plaz_second.setText(String.valueOf(ven_plaz_second));
         textView_result = findViewById(R.id.textViewresult);
+        back_button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent back_to_show_from_result = new Intent(ShowResultActivity.this, GetForCAlculateActivity.class);
+                startActivity(back_to_show_from_result);
+            }
+        });
     }
 }
